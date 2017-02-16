a1dfaaaf4574adb9b29d1b4d88ed83bdcec128d9
nickrally <nickrally2009@gmail.com>
2017-02-15 20:41:43 -0700

    minor changes to server

--- network/server.py
+++ network/server.py
@@ -3,10 +3,12 @@ import sys
 from threading import Thread
 import traceback
 
+
 def process_input(input_str):
     return input_str[::-1]
 
-def client_thread(conn, port, ip, MAX_BUFFER = 4096):
+
+def client_thread(conn, port, ip, MAX_BUFFER=4096):
     client_msg = conn.recv(MAX_BUFFER)
     msg_size = sys.getsizeof(client_msg)
     if msg_size >= MAX_BUFFER:
@@ -19,31 +21,40 @@ def client_thread(conn, port, ip, MAX_BUFFER = 4096):
     conn.sendall(result)
     conn.close()
     print('Connection ' + ip + ':' + port + ' closed')
-     
+
+
 def start_server():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server = '127.0.0.1'
-    port   =  5004
+    port = 5004
     try:
-        s.bind((server, port))
+        server_socket.bind((server, port))
     except:
         print('Bind failed. Error: ' + str(sys.exc_info()))
         sys.exit()
-    
-    s.listen(4)
-    print ('Listening...')
-    
+
+    server_socket.listen(4)
+    print('Listening...')
+
     while True:
-        conn, addr = s.accept()
+        '''
+        At accept(), a new socket is created that is distinct from the server_socket.
+        This new socket is used only for communication with a particular client.
+        For TCP servers, the socket object used to receive connections is not the same socket used
+        to perform subsequent communication with the client.
+        accept() returns a new socket obj that's actually used for the connection.
+        It allows a server to have connections with many clients simultaneously.
+        '''
+        client_socket, addr = server_socket.accept()
         ip, port = str(addr[0]), str(addr[1])
-        print ('Accepting connection from ' + ip + ':' + port)
+        print('Accepting connection from ' + ip + ':' + port)
         try:
-            Thread(target=client_thread, args=(conn, ip, port)).start()
+            Thread(target=client_thread, args=(client_socket, ip, port)).start()
         except:
             print('Oh,noes!')
             traceback.print_exc()
-    s.close()
+    server_socket.close()
 
 
 start_server()
